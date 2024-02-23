db867e1a2b9e88705cf440672b0202be209dd019
ma9pie <ma9pie.dev@gmail.com>
2024-02-23 16:08:48 +0900

    feat : test code 추가

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: "ts-jest",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
   transform: {
     "^.+\\.(ts|tsx)?$": "ts-jest",
     "^.+\\.(js|jsx)$": "babel-jest",
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
-const add = (a: number, b: number) => {
-  return a + b;
-};
-
-const sub = (a: number, b: number) => {
-  return a - b;
-};
-
-const mul = (a: number, b: number) => {
-  return a * b;
-};
-
-const div = (a: number, b: number) => {
-  return a / b;
-};
-
-export { add, sub, mul, div };
+export * from "./utils";
