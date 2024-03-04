45eb6743a4c851f50349d9b9d2ce159670151829
ma9pie <ma9pie.dev@gmail.com>
2024-03-04 20:22:27 +0900

    fix : vite.config.js 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,9 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import react from '@vitejs/plugin-react-swc';
-import libCss from 'vite-plugin-libcss';
-import dts from 'vite-plugin-dts';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react({ jsxImportSource: '@emotion/react' }),
-    dts({
-      insertTypesEntry: true,
-    }),
-    libCss(),
-  ],
+  plugins: [react({ jsxImportSource: '@emotion/react' })],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
-  },
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: '@ma9pie/test-package',
-      fileName: 'index',
-      formats: ['es', 'umd', 'cjs'],
-    },
-    rollupOptions: {
-      treeshake: 'recommended',
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-    sourcemap: true,
-    emptyOutDir: true,
   },
 });
