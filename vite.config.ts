af458a021a5270c00fc0851d0e7f992e664c4f2d
ma9pie <ma9pie.dev@gmail.com>
2024-03-04 19:48:53 +0900

    fix : component 배포 test

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react-swc';
+import libCss from 'vite-plugin-libcss';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true, // 컴포넌트 타입 생성
+    }),
+    libCss(),
+  ],
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: '@ma9pie/test-package',
+      fileName: 'index',
+      formats: ['es', 'umd', 'cjs'],
+    },
+    rollupOptions: {
+      treeshake: 'recommended',
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+});
