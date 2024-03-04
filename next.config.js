8f5e9db4da58ba7b0120599a370e189623aa9206
ma9pie <ma9pie.dev@gmail.com>
2024-03-04 19:27:57 +0900

    fix : next.config.js 수정

--- next.config.js
+++ next.config.js
@@ -3,15 +3,21 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: false,
   webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: 'ts-loader',
-          options: { transpileOnly: true },
-        },
-      ],
-    });
+    config.module.rules.push(
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: { transpileOnly: true },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    );
     return config;
   },
 };
