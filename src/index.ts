0f406406ea1dccd3463c2c04cf3faa31fab3143d
ma9pie <ma9pie.dev@gmail.com>
2024-03-05 19:23:31 +0900

    fix : script 수정

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 export * from './utils';
 export * from './hooks';
-// export * from './components';
-export { default as HelloWorld } from '@/components/HelloWorld';
-export { default as TestComponent } from '@/components/TestComponent';
+export * from './components';
